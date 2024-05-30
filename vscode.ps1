cc96646093b6c559f537f2380e2ad0139e6cee34
Keevan <keevandance@gmail.com>
2024-05-29 22:31:49 -0700

    Update scripts

--- vscode.ps1
+++ vscode.ps1
@@ -1,16 +1,21 @@
-$extensions =
-     "esbenp.prettier-vscode",
-     "tinkertrain.theme-panda"
+$LocalTempDir = $env:TEMP
+$VSCodeInstaller = "VSCodeInstaller.exe"
+$vscodeUrl = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
 
-$cmd = "code --list-extensions"
-Invoke-Expression $cmd -OutVariable output | Out-Null
-$installed = $output -split "\s"
+(new-object System.Net.WebClient).DownloadFile($vscodeUrl, "$LocalTempDir\$VSCodeInstaller") 
 
-foreach ($ext in $extensions) {
-    if ($installed.Contains($ext)) {
-        Write-Host $ext "already installed." -ForegroundColor Gray
-    } else {
-        Write-Host "Installing" $ext "..." -ForegroundColor White
-        code --install-extension $ext
-    }
-}
+&"$LocalTempDir\$VSCodeInstaller" /silent /install
+$Process2Monitor = "VSCodeInstaller"
+
+Do { 
+  $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name
+  If ($ProcessesFound) { 
+    "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 
+  } else { 
+    rm "$LocalTempDir\$ArcInstaller" -ErrorAction SilentlyContinue -Verbose 
+  } 
+} Until (!$ProcessesFound)
+
+# extension list:
+# prettier
+# panda theme
